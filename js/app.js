c73daa9c666d1e8dcc2f0470e9885ddf36b0b95d
Daniel F <daniel.flres@hotmail.com>
2026-09-14 15:22:36 -0300

    Estructura inicial del proyecto Servicio Envíos Express

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*
+    ========================================
+    SERVICIO ENVÍOS EXPRESS
+    Archivo principal de JavaScript
+    ========================================
+*/
+
+
+// Ejecutar mensaje
+console.log("Servicio Envíos Express se ha iniciado correctamente.");
